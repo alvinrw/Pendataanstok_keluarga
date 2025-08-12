7a477b22df933a5ed24517374992df3a3227938c
alvinrw <alvinrifky81@gmail.com>
2025-08-12 12:39:24 +0700

    minor

--- resources/views/manajemen/daftar_kloter.blade.php
+++ resources/views/manajemen/daftar_kloter.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Manajemen Kloter Pemeliharaan</title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link rel="stylesheet" href="{{ asset('css/daftar_kloter.css') }}">
+   
 <style>
     
         * { margin: 0; padding: 0; box-sizing: border-box; }
